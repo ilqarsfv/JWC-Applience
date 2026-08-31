f2d16928a6545493665847158939debe1d8c7b56
Ilqar <ilqar.sofiyev.95@gmail.com>
2026-08-31 13:37:02 +0400

    first com

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$(function () {
+  new Swiper(".bannerSwiper", {
+    navigation: {
+      nextEl: ".banner-swiper-button-next",
+      prevEl: ".banner-swiper-button-prev",
+    },
+    // loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+  });
+});
